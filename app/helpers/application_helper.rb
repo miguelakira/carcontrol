a5f3b09787d43e372d1a629c1424d9ba80327e4a
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2012-04-18 16:00:07 -0300

    adicionado css, ajustes no layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+# def button(name,url)
+  def button(*args)
+    if args.size == 2
+      name = args[0]
+      url = args[1]
+      content_tag :button, :type => "button", :onclick => "window.location.href =  '#{url_for(url)}'; " do
+        "#{name}"
+      end
+    elsif args.size == 1
+      name = args[0]
+      content_tag :button, :type => "button" do
+        "#{name}"
+      end
+    end
+  end
+
+
 end
