3c0003fb27308ff33c2021a88a67214a1f5a6d7c
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2012-05-08 18:07:06 -0300

    ajuste na migration

--- db/migrate/20120426184335_create_estados.rb
+++ db/migrate/20120426184335_create_estados.rb
@@ -1,7 +1,7 @@
 class CreateEstados < ActiveRecord::Migration
   def change
     create_table :estados do |t|
-    	t.string :nome,
+    	t.string :nome
     	t.string :sigla
       t.timestamps
     end
