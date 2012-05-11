5bd9b5971dff53e65bfa896326ec50424ad89099
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2012-05-11 15:50:47 -0300

    migration pra criar cidades

---
+++ db/migrate/20120508195841_create_cidades.rb
@@ -0,0 +1,10 @@
+class CreateCidades < ActiveRecord::Migration
+  def change
+    create_table :cidades do |t|
+      t.string :nome
+      t.integer :estado_id
+      t.integer :capital
+      t.timestamps
+    end
+  end
+end
