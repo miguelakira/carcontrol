7db29949d36212d56da16876b9d25283f16bc634
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2012-05-19 04:01:21 -0300

    ajustes no form dos carros

---
+++ db/mudar_isto_para_migration_create_cidades.rb
@@ -0,0 +1,11 @@
+class CreateCidades < ActiveRecord::Migration
+  def change
+    create_table :cidades do |t|
+      t.string :text
+      t.integer :estado_id
+      t.boolean :capital
+
+      t.timestamps
+    end
+  end
+end
