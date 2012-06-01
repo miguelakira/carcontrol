420828832cc41293787f52c1542fa4ab7456d0c8
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2012-06-01 16:26:52 -0300

    mudancas no layout

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(:version => 20120601031512) do
     t.integer  "cidade_destino"
     t.integer  "estado_origem"
     t.integer  "estado_destino"
-    t.integer  "ativo"
     t.integer  "cegonha_id"
+    t.integer  "ativo"
     t.integer  "comprador_id"
     t.integer  "empresa_id"
   end
@@ -53,12 +53,12 @@ ActiveRecord::Schema.define(:version => 20120601031512) do
   end
 
   create_table "cidades", :force => true do |t|
-    t.string   "text"
-    t.integer  "estado_id"
-    t.boolean  "capital"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string  "text",      :limit => 40, :null => false
+    t.integer "estado_id",               :null => false
+    t.boolean "capital",                 :null => false
   end
+
+  add_index "cidades", ["estado_id"], :name => "uf"
 
   create_table "compradores", :force => true do |t|
     t.string   "nome"
@@ -86,10 +86,10 @@ ActiveRecord::Schema.define(:version => 20120601031512) do
   end
 
   create_table "estados", :force => true do |t|
-    t.string   "nome"
-    t.string   "sigla"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.string   "nome",       :limit => 256, :null => false
+    t.string   "sigla",      :limit => 256, :null => false
   end
 
   create_table "status_pagamentos", :force => true do |t|
