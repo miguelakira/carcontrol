94c0035f557cf7fffb7f2165d11fb2d8e864b7bf
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2012-06-29 13:47:04 -0300

    ajustado pra aparecer carros com status 'aguardando carregamento' na lista de ativos

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -9,15 +9,15 @@ class CarsController < ApplicationController
      # ativo = true
      # @cars = sort_by_comprador(ativo)
     if params[:sort] == 'status_pagamento'
-      ativo = [1,2,3,4]
+      ativo = [1,2,3,4,5]
       @cars = sort_by_status_pagamento(ativo)
     elsif params[:sort] == 'saldo_total'
-      ativo = [1,2,3,4]
+      ativo = [1,2,3,4,5]
       @cars = sort_by_saldo_total(ativo)
     else
       
-      #@cars = Car.search(params[:search], params[:search_by]).order(:data_compra, :created_at).paginate(:per_page => 30, :page => params[:page]).where(:ativo => [1,2,3,4])
-      @cars = Car.search(params[:search], params[:search_by]).order(sort_column + ' ' + sort_direction).paginate(:per_page => 30, :page => params[:page]).where(:ativo => [1,2,3,4])
+      #@cars = Car.search(params[:search], params[:search_by]).order(:data_compra, :created_at).paginate(:per_page => 30, :page => params[:page]).where(:ativo => [1,2,3,4,5])
+      @cars = Car.search(params[:search], params[:search_by]).order(sort_column + ' ' + sort_direction).paginate(:per_page => 30, :page => params[:page]).where(:ativo => [1,2,3,4,5])
       @cars.each do |car|
         if car.nome.nil?
           if car.comprador
