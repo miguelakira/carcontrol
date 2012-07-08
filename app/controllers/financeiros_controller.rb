6ecdb5880087c98f117e74f222309b9efc90c040
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2012-07-08 03:04:54 -0300

    mais informacoes no controle financeiro

--- app/controllers/financeiros_controller.rb
+++ app/controllers/financeiros_controller.rb
@@ -18,5 +18,15 @@ class FinanceirosController < ApplicationController
   		@cars_ativos = @cars_cpf.reject {|c| c.ativo == 0}
   		@cars_inativos = @cars_cpf.reject {|c| c.ativo != 0}
   	end
+
+  	unless @cars_ativos.nil?
+  		@valor_total = 0
+      @valor_pago = 0
+  		@cars_ativos.each do |car|
+  			@valor_total += car.pagamento.valor_total unless car.pagamento.valor_total.nil?
+        @valor_pago += car.pagamento.valor_pago unless car.pagamento.valor_pago.nil?
+  		end
+    end	
   end
+
 end
