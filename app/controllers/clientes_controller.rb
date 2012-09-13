7f80bdff939b2ea28d307ba96ce86e54e30887c1
Miguel Akira <miguel.akira@gmail.com>
2012-09-13 13:54:30 -0300

    ajuste na view do cliente

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -8,12 +8,31 @@ class ClientesController < ApplicationController
   end
 
   def show
-    cliente = Comprador.find_by_cpf(params[:comprador_cpf])
-    if cliente.nil?
+    @cliente = Comprador.find_by_cpf(params[:comprador_cpf])
+
+    if @cliente.nil?
         @mensagem = "<p><h1>CPF não encontrado.</h1></p> <p><b>Por favor verifique se todos os dados estão corretos ou entre em contato com a Transroyal.</b></p>"
     else        
-        @cars = cliente.cars
-        @mensagem = "<p><h1>Bem-vindo, #{cliente.nome}.</h1></p>"
+      @cars = @cliente.cars
+      @mensagem = "<p><h1>Bem-vindo, #{@cliente.nome}.</h1></p>"
+
+      @cars_ativos = @cars.reject {|c| c.ativo == 0}
+      @cars_inativos = @cars.reject {|c| c.ativo != 0}
+      @cars_nao_pagos = @cars.reject {|c| c.status_pagamento_id == 3 }
+      @cars_pagos = @cars.reject {|c| c.status_pagamento_id != 3 }
+
+      unless @cars_ativos.nil?
+        @valor_total = 0
+        @valor_pago = 0
+
+        unless @cars.nil?
+          @cars.each do |car|
+            @valor_total += car.pagamento.valor_total unless car.pagamento.valor_total.nil?
+            @valor_pago += car.pagamento.valor_pago unless car.pagamento.valor_pago.nil?
+          end
+        end
+      @saldo_devedor = @valor_total - @valor_pago
+      end
     end
   end
 end
