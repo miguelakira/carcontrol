dd779b910f56c2e2a5f1e0fced525ab1511c6165
Miguel Akira <miguel.akira@gmail.com>
2012-09-19 12:50:56 -0300

    ajustes no layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    @user.save
+    redirect_to @user
+
+  end
+
   def update
     @user = User.find(params[:id])
     respond_to do |format|
