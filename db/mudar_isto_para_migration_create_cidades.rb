ab09eeea92525ec886290d4196b60596900e7847
Miguel Akira <miguel.akira@gmail.com>
2012-09-20 13:48:34 -0300

    unless exists no db

--- db/mudar_isto_para_migration_create_cidades.rb
+++ db/mudar_isto_para_migration_create_cidades.rb
@@ -1,11 +1,11 @@
 class CreateCidades < ActiveRecord::Migration
-  def change
-    create_table :cidades do |t|
-      t.string :text
-      t.integer :estado_id
-      t.boolean :capital
-
-      t.timestamps
-    end
-  end
+#  def change
+#    create_table :cidades do |t|
+#      t.string :text
+#      t.integer :estado_id
+#      t.boolean :capital
+#
+#      t.timestamps
+#    end
+#  end
 end
