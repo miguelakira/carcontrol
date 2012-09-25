f343c364f426e04775f1af5bfae89ee905a58e99
Miguel Akira <miguel.akira@gmail.com>
2012-09-25 17:49:07 -0300

    ajuste na visualizacao dos parceiros

--- app/models/parceiro.rb
+++ app/models/parceiro.rb
@@ -1,11 +1,19 @@
 class Parceiro < ActiveRecord::Base
-  attr_accessible :carros, :celular, :cidade_destino, :cidade_id, :cidade_origem, :observacao, :contato, 
+  attr_accessible :carros, :celular, :cidade_destino, :cidade_id, :cidade_origem, :observacao, :contato,
   		:estado_destino, :estado_id, :estado_origem, :localizacao, :nome, :telefone, :cnpj, :email, :cpf
   has_one :pagamento
   has_many :cars
   has_many :historicos
 
-  validates	:nome, 
+  validates	:nome,
   			:presence => { :message => "- O nome nao pode ser deixada em branco!" }
-  			
+
+  before_save :titleize_nome
+
+  def titleize_nome
+    if self.nome
+      self.nome = self.nome.titleize
+    end
+  end
+
 end
