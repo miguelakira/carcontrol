6386c2b6635cfed27d4dc798a51bfe68a67da7bb
Miguel Akira <miguel.akira@gmail.com>
2012-09-25 17:19:30 -0300

    ajuste no css dos carros nao pagos

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,12 +1,12 @@
 // Avisa que é possível escolher somente parceiro ou cegonha, não ambos
 function parceiroOrCegonhaNotBoth(changedSelect, selectId) {
-    
+
   //alert(changedSelect.options[changedSelect.selectedIndex].value);
    var otherSelect = document.getElementById(selectId);
     if(changedSelect.options[changedSelect.selectedIndex].value != ''){
       if(otherSelect.selectedIndex != 0) {
       var answer = confirm("Só é possível escolher Cegonha ou Parceiro, mas não ambos. Ao selecionar esta opção, vocẽ irá remover o carro do outro");
-          if(answer) { 
+          if(answer) {
             otherSelect.selectedIndex = 0;
           }
           else {
@@ -14,6 +14,6 @@ function parceiroOrCegonhaNotBoth(changedSelect, selectId) {
           }
       }
     }
-    
+
 }
 
