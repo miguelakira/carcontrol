5406abf9b923e9198018458631a9cbcbcacad382
Miguel Akira <miguel.akira@tagview.com.br>
2014-05-29 19:09:02 -0300

    tentando ainda solucionar problema no heroku

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ end
 
 group :production do
 	gem 'pg'
+  gem 'rails_12factor'
 end
 
 group :test do
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require dataTables/jquery.dataTables
+//= require dataTables
 //= require dataTables.foundation
 //= require_tree .
 $(function() {
