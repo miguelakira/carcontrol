6129e35a3d5c2e14ca28a638d0f49f0588c7ed9e
Miguel Akira <miguel.akira@tagview.com.br>
2014-07-07 11:25:34 -0300

    Removendo métodos inuteis de application controller e exibindo corretamente historico de cegonha

--- app/models/change.rb
+++ app/models/change.rb
@@ -3,4 +3,21 @@ class Change < ActiveRecord::Base
 
   default_scope order('created_at DESC')
   belongs_to :tracking
+
+  def previous_city
+    previous_location ? Cidade.find(previous_location).try(:text) : ""
+  end
+
+  def current_city
+    Cidade.find(current_location).try(:text)
+  end
+
+  def previous_state
+    previous_location ? Cidade.find(previous_location).try(:estado).sigla : ""
+  end
+
+  def current_state
+  Cidade.find(current_location).try(:estado).sigla
+  end
+
 end
