6ad56d769a474e2166ef864d66e57b8612d49d7e
Miguel Akira <miguel.akira@gmail.com>
2016-05-05 13:28:38 -0300

    enabling access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :redirect_to_disabled
+  # before_filter :redirect_to_disabled
 
 
   private
-  def redirect_to_disabled
-    redirect_to root_path
-  end
+  # def redirect_to_disabled
+  #   redirect_to root_path
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Carcontrol::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-  root :controller => "static", :action => "/"
+  root :to => 'clientes#index'
 
   # See how all your routes lay out with "rake routes"
 
