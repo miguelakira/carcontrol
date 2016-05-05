ce422a8a9a01eb387dd5b150958362fec467d127
Miguel Akira <miguel.akira@gmail.com>
2016-05-05 11:24:22 -0300

    disabling access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :redirect_to_disabled
+
+
+  private
+  def redirect_to_disabled
+    redirect_to root_path
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,20 +25,20 @@ Carcontrol::Application.routes.draw do
   end
 
   resources :parceiros
-  devise_for :users, :path_prefix => 'd'
+  devise_for :users, :path_prefix => "d"
   resources :users
   resources :clientes
   resources :financeiros
-  match ':controller(/:action(/:id))(.:format)'
+  match ":controller(/:action(/:id))(.:format)"
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-  root :to => 'clientes#index'
+  root :controller => "static", :action => "/"
 
   # See how all your routes lay out with "rake routes"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # This is a legacy wild controller route that"s not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  # match ":controller(/:action(/:id))(.:format)"
 end
